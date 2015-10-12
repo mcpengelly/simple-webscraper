a97e57397ebdb7e957beb8df7955f12df053c84d
Matthew Pengelly <pengelly.mat@gmail.com>
2015-10-12 13:13:03 -0400

    imported project

---
+++ app.js
@@ -0,0 +1,42 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var prompt = require('prompt');
+var fs = require('fs');
+
+prompt.start();
+
+var url = 'XXXXX';
+
+prompt.get(['url'], function(err, result){
+	if (err) throw err;
+	url = result.url;
+});
+
+request(url, function(err, res, html) {
+	if (!err) {
+		var title, datePublished, rating;
+		var json = { title: '', datePublished: '', rating: '' };
+		var $ = cheerio.load(html);
+
+		$('.header').filter(function() {
+			var element = $(this);
+			//traverse DOM to find title, publishDate and rating
+			title = element.children().first().text();
+			datePublished = element.children().last().text();
+
+			json.title = title;
+			json.datePublished = datePublished;
+		});
+
+		$('.star-box-giga-star').filter(function() {
+			var element = $(this);
+			rating = element.text().trim();
+			json.rating = rating;
+		});
+
+		fs.writeFile('theMartianIMBD.json', JSON.stringify(json, null, 4), function(err) {
+			if (err) throw err;
+			console.log("File Written to project directory");
+		});
+	}
+});
