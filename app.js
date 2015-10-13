579c01fb713f09bf18d4edaf89617f9ad53c333d
Matthew Pengelly <pengelly.mat@gmail.com>
2015-10-13 18:58:54 -0400

    prompt user for url

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var cheerio = require('cheerio');
 var request = require('request');
-var prompt = require('prompt');
 var fs = require('fs');
+var readlineSync = require('readline-sync');
 
-//prompt.start();
+var url, validUrl;
 
-var url = 'http://www.imdb.com/title/tt3659388/';
+//user must enter valid url to proceed scraping
+do {
+	url = readlineSync.question('Enter an imdb movie url: ');
+	validUrl = url.indexOf('www.imdb');
+} while(validUrl === -1);
 
-/*prompt.get(['url'], function(err, result){
-	if (err) throw err;
-	url = result.url;
-});*/
-
+//request to url user provides
 request(url, function(err, res, html) {
 	if (!err) {
 		var title, datePublished, rating, description;
@@ -20,7 +20,7 @@ request(url, function(err, res, html) {
 
 		$('.header').filter(function() {
 			var element = $(this);
-			//traverse DOM to find title, publishDate and rating
+			//traverse DOM to find title, publishDate
 			title = element.children().first().text();
 			datePublished = element.children().last().text();
 
@@ -30,17 +30,19 @@ request(url, function(err, res, html) {
 
 		$("#title-overview-widget").filter(function() {
 			var element = $(this);
+			//traverse DOM to find description
 			description = element.children().find('p').first().next().text().trim();
 			json.description = description;
 		});
 
 		$('.star-box-giga-star').filter(function() {
 			var element = $(this);
+			//find rating
 			rating = element.text().trim();
 			json.rating = rating;
 		});
 
-		fs.writeFile('theMartianIMBD.json', JSON.stringify(json, null, 4), function(err) {
+		fs.writeFile('movieInfo.json', JSON.stringify(json, null, 4), function(err) {
 			if (err) throw err;
 			console.log("File Written to project directory");
 		});
